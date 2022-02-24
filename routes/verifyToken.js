7fd06e39b1632d79205332c09924123ce8b08c2d
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2022-02-25 00:11:26 +0530

    payment setup

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -24,4 +24,18 @@ const verifyTokenAuthorization = (req, res, next) => {
   });
 };
 
-module.exports = {verifyToken, verifyTokenAuthorization};
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAuthorization,
+  verifyTokenAndAdmin,
+};
